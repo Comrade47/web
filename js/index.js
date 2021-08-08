99e3bbde74b585e84c54c80eb9782eea4015b40d
Spondon <86315102+Comrade47@users.noreply.github.com>
2021-08-08 20:38:43 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	$('.nav-icon a').click(function(){
+        $('.main-menu nav').slideToggle(800);
+    })
+
+    // banner slider
+    $('.banner-slider').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:false,
+        autoplay:true,
+        infinite:true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots:false,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll:1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+})
